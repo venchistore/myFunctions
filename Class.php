da8c70ff611b707c7cca8dea19101ee28bba03ad
venchistore <venchistore08@gmail.com>
2024-12-26 03:49:18 +0700

    Update Class.php

--- Class.php
+++ Class.php
@@ -117,7 +117,7 @@ class Display {
 
 class Functions {
 	static $configFile = "config.json";
-	static function Tmr($tmr){date_default_timezone_set("UTC");$sym = [' ─ ',' カウントを待っています ',];$timr = time()+$tmr;$a = 0;while(true){$a +=1;$res=$timr-time();if($res < 1) {break;}print $sym[$a % 4].p.date('H',$res).":".p.date('i',$res).":".p.date('s',$res)."\r";usleep(100000);}print "\r           \r";}
+	static function Tmr($tmr){date_default_timezone_set("UTC");$sym = [' ─ ',' カウントを待っています ',' │ ',' カウントを待っています ',];$timr = time()+$tmr;$a = 0;while(true){$a +=1;$res=$timr-time();if($res < 1) {break;}print $sym[$a % 4].p.date('H',$res).":".p.date('i',$res).":".p.date('s',$res)."\r";usleep(100000);}print "\r           \r";}
 	static function Server($title){$url = "https://iewilbot.my.id/List/server.php";$param = "title=".$title;$r = file_get_contents($url."?".$param);return json_decode($r,1);}
 	static function setConfig($key){if(!file_exists(self::$configFile)){$config = [];}else{$config = json_decode(file_get_contents(self::$configFile),1);}if(isset($config[$key])){return $config[$key];}else{print Display::isi($key);$data = readline();print n;$config[$key] = $data;file_put_contents(self::$configFile,json_encode($config,JSON_PRETTY_PRINT));return $data;}}
 	static function removeConfig($key){$config = json_decode(file_get_contents(self::$configFile),1);unset($config[$key]);file_put_contents(self::$configFile,json_encode($config,JSON_PRETTY_PRINT));}
